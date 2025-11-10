3af1f6dcc8971e6c4f778a073ba7fd0599c7339d
JPS <jpsosa_ar@yahoo.com>
2025-11-09 21:09:51 -0300

    fix app.js en fetch

--- public/js/app.js
+++ public/js/app.js
@@ -26,7 +26,8 @@ weatherForm.addEventListener('submit',(e) => {
     messageFive.textContent = ''
     messageSix.textContent = ''
     messageSeven.textContent = ''
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+//  fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('https://weatherv03-production.up.railway.app/weather?address='+location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error)
