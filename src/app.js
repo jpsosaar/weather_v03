1338a693f77e21b01a1cc331dcbe0287d842a419
JPS <jpsosa_ar@yahoo.com>
2025-11-15 18:22:43 -0300

    puerto por Environment en vez de fijo 3

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const forecast = require('./utils/nforecast3.js')
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-//const { error } = require('console')
+const { error } = require('console')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -132,7 +132,7 @@ app.get('*', (req, res) => {
 
 // Start up the Server
 app.listen(port, () => {
-    console.log('Server is up on port: ' + port)
+    console.log('Server is up on port ' + port)
 })
 
 
